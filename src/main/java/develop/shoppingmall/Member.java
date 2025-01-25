953c93932241c307f63bf9e121b349c5aff0b34e
MinseokGo <rhalstjr1999@naver.com>
2025-01-25 18:40:03 +0900

    feat: 멤버 엔티티 등록

---
+++ src/main/java/develop/shoppingmall/Member.java
@@ -0,0 +1,53 @@
+package develop.shoppingmall;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 16, nullable = false)
+    private String name;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 512, nullable = false)
+    private String password;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private boolean status;
+
+    protected Member() {
+    }
+
+    protected Member(
+            String name,
+            String email,
+            String password
+    ) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.status = true;
+    }
+}
