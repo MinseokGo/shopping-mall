f7fe25e3b73d86de768c8d5fba28c50418fe6ed6
MinseokGo <rhalstjr1999@naver.com>
2025-01-25 17:52:10 +0900

    init: 프로젝트 세팅

---
+++ src/test/java/develop/shoppingmall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package develop.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
