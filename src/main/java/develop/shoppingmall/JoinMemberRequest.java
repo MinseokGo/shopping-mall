940fe692f243c8a0c1516c43731857a0ccde284f
MinseokGo <rhalstjr1999@naver.com>
2025-02-03 17:11:51 +0900

    feat: 회원가입 요청 DTO 등록

---
+++ src/main/java/develop/shoppingmall/JoinMemberRequest.java
@@ -0,0 +1,23 @@
+package develop.shoppingmall;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record JoinMemberRequest(
+
+        @NotBlank(message = "이름을 입력해주세요.")
+        @Size(min = 2, max = 16, message = "이름은 2자 이상 16자 이하여야 합니다.")
+        String name,
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Size(min = 2, max = 128, message = "이메일은 2자 이상 128자 이하여야 합니다.")
+        @Email(message = "이메일 형식으로 입력해주세요.")
+        String email,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Size(min = 8, max = 512, message = "비밀번호는 8자 이상 512자 이하여야 합니다.")
+        String password
+
+) {
+}
