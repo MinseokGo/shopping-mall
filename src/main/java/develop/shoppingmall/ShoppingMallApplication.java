185c9b3adad47e82aa557a21b1fcd84ebd7f9f5d
MinseokGo <rhalstjr1999@naver.com>
2025-02-03 17:09:54 +0900

    style: 코드 포맷팅

--- src/main/java/develop/shoppingmall/ShoppingMallApplication.java
+++ src/main/java/develop/shoppingmall/ShoppingMallApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class ShoppingMallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingMallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallApplication.class, args);
+    }
 }
