8be32383bc54c6f7d1a89a422bb42252ef449cee
MinseokGo <rhalstjr1999@naver.com>
2025-02-13 14:40:04 +0900

    test: 수동 트랜잭션 추가

--- src/test/java/develop/shoppingmall/member/MemberControllerTest.java
+++ src/test/java/develop/shoppingmall/member/MemberControllerTest.java
@@ -4,6 +4,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 import io.restassured.http.ContentType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +23,29 @@ public class MemberControllerTest {
     private int port;
 
     @Autowired
-    MemberController memberController;
+    EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+        try {
+            memberRepository.deleteAll();
+            entityManager.flush();
+            entityManager.clear();
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            entityManager.close();
+        }
+    }
 
     @Test
     @DisplayName("멤버 회원가입 통합 테스트")
@@ -34,5 +60,31 @@ public class MemberControllerTest {
                 .then()
                 .statusCode(201)
                 .body(equalTo("회원가입이 완료 되었습니다."));
+    }
+
+    @Test
+    @DisplayName("멤버 회원가입 실패 통합 테스트")
+    void test1() {
+        JoinMemberRequest oldRequest = new JoinMemberRequest("testName", "testEmail@email.com", "testPassword");
+        given()
+                .baseUri("http://localhost:" + port)
+                .contentType(ContentType.JSON)
+                .body(oldRequest)
+                .when()
+                .post("/members")
+                .then()
+                .statusCode(201)
+                .body(equalTo("회원가입이 완료 되었습니다."));
+
+        JoinMemberRequest newRequest = new JoinMemberRequest("testName", "testEmail@email.com", "testPassword");
+        given()
+                .baseUri("http://localhost:" + port)
+                .contentType(ContentType.JSON)
+                .body(newRequest)
+                .when()
+                .post("/members")
+                .then()
+                .statusCode(409)
+                .body(equalTo("[ERROR] 이미 존재하는 이메일입니다."));
     }
 }
