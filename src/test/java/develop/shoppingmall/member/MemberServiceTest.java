d125d55a77deb363af4298bdd6840876399ef561
MinseokGo <rhalstjr1999@naver.com>
2025-02-13 13:48:36 +0900

    test: 회원가입 서비스 테스트

---
+++ src/test/java/develop/shoppingmall/member/MemberServiceTest.java
@@ -0,0 +1,44 @@
+package develop.shoppingmall.member;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        Member member = new Member("testName", "testEmail", "testPassword");
+        memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 이메일로 회원가입 시도 시 예외 처리 테스트")
+    void test0() {
+        JoinMemberRequest request = new JoinMemberRequest("testName", "testEmail", "testPassword");
+        assertThatThrownBy(() -> memberService.join(request))
+                .isInstanceOf(AlreadyExistMemberEmailException.class);
+    }
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void test1() {
+        JoinMemberRequest request = new JoinMemberRequest("testName", "newEmail", "testPassword");
+        memberService.join(request);
+    }
+}
