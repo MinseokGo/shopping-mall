f5cfce3cae0273ae9ae6e17f60350239050ae58b
MinseokGo <rhalstjr1999@naver.com>
2025-02-13 13:48:23 +0900

    test: 테스트 오류 수정

--- src/test/java/develop/shoppingmall/member/MemberRepositoryTest.java
+++ src/test/java/develop/shoppingmall/member/MemberRepositoryTest.java
@@ -1,30 +1,32 @@
 package develop.shoppingmall.member;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class MemberRepositoryTest {
 
+    @Autowired
     MemberRepository memberRepository;
 
-    @Autowired
-    MemberRepositoryTest(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    @BeforeEach
+    void setUp() {
+        Member member = new Member("testName", "testEmail", "testPassword");
+        memberRepository.save(member);
     }
 
     @Test
     @DisplayName("이미 존재하는 이메일 검증 테스트")
     void test0() {
-        Member member = new Member("testName", "testEmail", "testPassword");
-        memberRepository.save(member);
-
         assertThat(
-                memberRepository.existsByEmail(member.getEmail())
+                memberRepository.existsByEmail("testEmail")
         ).isTrue();
     }
 }
