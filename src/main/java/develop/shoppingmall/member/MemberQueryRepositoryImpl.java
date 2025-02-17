2f40a2a417c928074344081cdc061db921dd9af2
MinseokGo <rhalstjr1999@naver.com>
2025-02-17 15:53:30 +0900

    feat: 멤버 비밀번호 조회 Query 메서드 등록

---
+++ src/main/java/develop/shoppingmall/member/MemberQueryRepositoryImpl.java
@@ -0,0 +1,34 @@
+package develop.shoppingmall.member;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+class MemberQueryRepositoryImpl implements MemberQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QMember member;
+
+    MemberQueryRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        this.jpaQueryFactory = jpaQueryFactory;
+        this.member = QMember.member;
+    }
+
+    @Override
+    public FindLoginMemberDTO findPassword(String email) {
+        return Optional.ofNullable(
+                        jpaQueryFactory
+                                .select(
+                                        Projections.constructor(
+                                                FindLoginMemberDTO.class,
+                                                member.password
+                                        )
+                                )
+                                .from(member)
+                                .where(member.email.eq(email))
+                                .fetchOne()
+                )
+                .orElseThrow(() -> new EntityNotFoundException("[ERROR] 멤버 정보를 찾을 수 없습니다."));
+    }
+}
