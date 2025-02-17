55fce64ce9ae086c114d7381d87ac9709d53d56c
MinseokGo <rhalstjr1999@naver.com>
2025-02-17 15:54:03 +0900

    feat: Query DSL configuration 등록

---
+++ src/main/java/develop/shoppingmall/QueryDSLConfig.java
@@ -0,0 +1,21 @@
+package develop.shoppingmall;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDSLConfig {
+
+    private final EntityManager entityManager;
+
+    QueryDSLConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
