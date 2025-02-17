65adb91793f80f79bc9afedc4503df3b4bf0c6dc
MinseokGo <rhalstjr1999@naver.com>
2025-02-17 15:53:46 +0900

    refactor: 쿼리 리포지토리 import

--- src/main/java/develop/shoppingmall/member/MemberRepository.java
+++ src/main/java/develop/shoppingmall/member/MemberRepository.java
@@ -1,8 +1,11 @@
 package develop.shoppingmall.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface MemberRepository extends JpaRepository<Member, Long> {
+interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryRepository {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
