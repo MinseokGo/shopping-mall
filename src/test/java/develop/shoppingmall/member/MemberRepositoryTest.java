6098d44ecd494d0700b956a8b3bbe1c7a614b9bd
MinseokGo <rhalstjr1999@naver.com>
2025-02-17 15:54:15 +0900

    test: QueryDSL config 등록

--- src/test/java/develop/shoppingmall/member/MemberRepositoryTest.java
+++ src/test/java/develop/shoppingmall/member/MemberRepositoryTest.java
@@ -2,13 +2,16 @@ package develop.shoppingmall.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import develop.shoppingmall.QueryDSLConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+@Import(QueryDSLConfig.class)
 @DataJpaTest
 @ActiveProfiles("test")
 class MemberRepositoryTest {
