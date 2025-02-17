186518d7b067fa3125299d364dd14444cc175b4a
MinseokGo <rhalstjr1999@naver.com>
2025-02-17 16:19:41 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/develop/shoppingmall/member/repository/MemberRepository.java
+++ src/main/java/develop/shoppingmall/member/repository/MemberRepository.java
@@ -1,12 +1,9 @@
 package develop.shoppingmall.member.repository;
 
 import develop.shoppingmall.member.domain.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryRepository {
 
     boolean existsByEmail(String email);
-
-    Optional<Member> findByEmail(String email);
 }
