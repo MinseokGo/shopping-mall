31d56337bbae0972aebadafaaf0ba932b813e648
MinseokGo <rhalstjr1999@naver.com>
2025-02-24 21:40:34 +0900

    refactor: 로그인 API 이동

--- src/main/java/develop/shoppingmall/member/controller/MemberController.java
+++ src/main/java/develop/shoppingmall/member/controller/MemberController.java
@@ -1,13 +1,10 @@
 package develop.shoppingmall.member.controller;
 
 import develop.shoppingmall.member.controller.dto.JoinMemberRequest;
-import develop.shoppingmall.member.controller.dto.LoginMemberRequest;
 import develop.shoppingmall.member.service.MemberService;
 import jakarta.validation.Valid;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,19 +18,8 @@ public class MemberController {
 		this.memberService = memberService;
 	}
 
-	@GetMapping("/members")
-	ResponseEntity<String> login(
-			@Valid @RequestBody LoginMemberRequest request
-	) {
-		memberService.login(request);
-		return ResponseEntity.ok()
-				.body("로그인에 성공 하였습니다.");
-	}
-
 	@PostMapping("/members")
-	ResponseEntity<String> join(
-		@Valid @RequestBody JoinMemberRequest request
-	) {
+	ResponseEntity<String> join(@Valid @RequestBody JoinMemberRequest request) {
 		memberService.join(request);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body("회원가입이 완료 되었습니다.");
