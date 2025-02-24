1c8f4303b4e0a4b520648adaca205f122a891823
MinseokGo <rhalstjr1999@naver.com>
2025-02-24 21:56:34 +0900

    test: 토큰 파싱 실패 테스트

--- src/test/java/develop/shoppingmall/auth/JwtServiceTest.java
+++ src/test/java/develop/shoppingmall/auth/JwtServiceTest.java
@@ -1,6 +1,7 @@
 package develop.shoppingmall.auth;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class JwtServiceTest {
+
+    String token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaW5zZW9rIEdvIiwiaXNzIjoiTWluc2VvayBHbyBzaG9wcGluZyBtYWxsIHNlcnZlciIsImp0aSI6InRlc3RFbWFpbCIsImVtYWlsIjoidGVzdEVtYWlsIiwiaWF0IjoxNzQwNDAwOTEzLCJleHAiOjE3NDA0ODczMTN9.pOjwkGWqA0MDbN0_cHx-KZN_QRb_9CrcBBdJSFK5b_g";
 
     @Autowired
     JwtService jwtService;
@@ -25,7 +28,13 @@ class JwtServiceTest {
     @Test
     @DisplayName("JWT 파싱 테스트")
     void test1() {
-        jwtService.parse(
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaW5zZW9rIEdvIiwiaXNzIjoiTWluc2VvayBHbyBzaG9wcGluZyBtYWxsIHNlcnZlciIsImp0aSI6InRlc3RFbWFpbCIsImVtYWlsIjoidGVzdEVtYWlsIiwiaWF0IjoxNzQwNDAwOTEzLCJleHAiOjE3NDA0ODczMTN9.pOjwkGWqA0MDbN0_cHx-KZN_QRb_9CrcBBdJSFK5b_g");
+        jwtService.parse(token);
+    }
+
+    @Test
+    @DisplayName("실패 - 유효하지 않은 JWT")
+    void test2() {
+        assertThatThrownBy(() -> jwtService.parse(token.substring(1)))
+                .hasMessage("[ERROR] 유효하지 않은 토큰입니다.");
     }
 }
