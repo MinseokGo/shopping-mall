38d96a350b172c58a29d19b7ec3ab370d866abc9
MinseokGo <rhalstjr1999@naver.com>
2025-03-11 16:58:50 +0900

    feat: member 탈퇴 api 작성

--- src/main/java/develop/shoppingmall/member/controller/MemberController.java
+++ src/main/java/develop/shoppingmall/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package develop.shoppingmall.member.controller;
 
+import develop.shoppingmall.auth.AuthService;
 import develop.shoppingmall.member.controller.dto.JoinMemberRequest;
 import develop.shoppingmall.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
+	private final AuthService authService;
 	private final MemberService memberService;
 
-	MemberController(MemberService memberService) {
+	MemberController(MemberService memberService, AuthService authService) {
+		this.authService = authService;
 		this.memberService = memberService;
 	}
 
@@ -23,5 +28,13 @@ public class MemberController {
 		memberService.join(request);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body("회원가입이 완료 되었습니다.");
+	}
+
+	@DeleteMapping("/members")
+	ResponseEntity<String> deleteMember(HttpServletRequest request) {
+		String email = authService.authenticate(request);
+		memberService.delete(email);
+		return ResponseEntity.ok()
+				.body("회원 탈퇴가 정상적으로 처리되었습니다.");
 	}
 }
